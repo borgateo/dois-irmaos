7460e3e9c14fbfb70b14b663c7128f1f44792333
Matteo Borgato <borgato.matteo@gmail.com>
2013-05-27 10:28:37 -0300

    init

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Rails3MongoidDevise::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Aggiunto per il test in localhost
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
