53112e180dc86c9d61541da89004352003e9cb89
Matteo Borgato <borgato.matteo@gmail.com>
2013-05-29 13:59:52 -0300

    added html5 BP, compass and Pure YUI css

--- Gemfile
+++ Gemfile
@@ -18,9 +18,12 @@ group :assets do
   # gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails'
+  gem 'compass-h5bp'
 end
 
 gem 'jquery-rails'
+gem 'html5-rails'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module FattiEPupe
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
+    # Font-face
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
