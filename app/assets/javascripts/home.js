1c505838017c7dec87fd3a4596d10907bb7944bb
Matteo Borgato <borgato.matteo@gmail.com>
2013-07-05 00:08:18 -0300

    added groups for the multimedia section

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,21 @@ $(function() {
   $("body").removeClass("preload");
   $('.loading').removeClass('loading');
 
-  $("img").unveil();
+  if( $('#multimedia').length > 0 ) {
+    
+    $('.group h3').on('click', function() {
+      var content = $(this).closest('.group').children('.group-content');
+      var img     = content.find("img");
+
+      if ( content.hasClass('active') ) {
+        content.slideUp().removeClass('active');
+      } else {
+        content.slideDown(function() {
+          img.unveil();
+        }).addClass('active');
+      }
+    })
+  
+  }
   
 });
